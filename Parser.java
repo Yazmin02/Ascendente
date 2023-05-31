d8af159dbf9e5f954608b1f056cd58b64e698928
Yazmin02 <ygonzalezm1704@alumno.ipn.mx>
2023-05-31 03:03:27 -0600

    Update Parser.java

---
+++ Parser.java
@@ -0,0 +1,62 @@
+
+import java.util.List;
+
+public class Parser {
+
+    private final List<Token> tokens;
+
+    private final Token identificador = new Token(TipoToken.IDENTIFICADOR, "");
+    private final Token select = new Token(TipoToken.SELECT, "select");
+    private final Token from = new Token(TipoToken.FROM, "from");
+    private final Token distinct = new Token(TipoToken.DISTINCT, "distinct");
+    private final Token coma = new Token(TipoToken.COMA, ",");
+    private final Token punto = new Token(TipoToken.PUNTO, ".");
+    private final Token asterisco = new Token(TipoToken.ASTERISCO, "*");
+    private final Token finCadena = new Token(TipoToken.EOF, "");
+
+    private int i = 0;
+    private boolean hayErrores = false;
+
+    private Token preanalisis;
+
+    public Parser(List<Token> tokens){
+        this.tokens = tokens;
+    }
+
+    public void parse(){
+        i = 0;
+        preanalisis = tokens.get(i);
+        
+
+        if(!hayErrores && !preanalisis.equals(finCadena)){
+            System.out.println("Error en la posición " + preanalisis.posicion + ". No se esperaba el token " + preanalisis.tipo);
+        }
+        else if(!hayErrores && preanalisis.equals(finCadena)){
+            System.out.println("Consulta válida");
+        }
+
+        /*if(!preanalisis.equals(finCadena)){
+            System.out.println("Error en la posición " + preanalisis.posicion + ". No se esperaba el token " + preanalisis.tipo);
+        }else if(!hayErrores){
+            System.out.println("Consulta válida");
+        }*/
+    }
+
+    
+
+
+    void coincidir(Token t){
+        if(hayErrores) return;
+
+        if(preanalisis.tipo == t.tipo){
+            i++;
+            preanalisis = tokens.get(i);
+        }
+        else{
+            hayErrores = true;
+            System.out.println("Error en la posición " + preanalisis.posicion + ". Se esperaba un  " + t.tipo);
+
+        }
+    }
+
+}
